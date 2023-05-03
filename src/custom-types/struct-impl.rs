904d3ac83127c672e72da4566ff96b77b2d359e1
Wenderson Pires <wendersonpdas@gmail.com>
2023-05-03 12:54:25 -0300

    struct & impl

---
+++ src/custom-types/struct-impl.rs
@@ -0,0 +1,19 @@
+struct Person {
+  name: String,
+  age: i32,
+}
+
+impl Person {
+  fn sayhi(&self) -> String {
+      // faz a mesma coisa que o println! porem retorna o valor
+      format!("Meu nome e {} e eu tenho {} anos", self.name, self.age)
+  } 
+}
+
+fn main() {
+  let person = Person {name: "Bruno".to_string(), age: 32};
+  let person2 = Person {name: "Flavia".to_string(), age: 26};
+
+  println!("{}", person.sayhi());
+  print!("{}", person2.sayhi());
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
-mod my_mod;
+struct Person {
+    name: String,
+    age: i32,
+}
+
+impl Person {
+    fn sayhi(&self) -> String {
+        // faz a mesma coisa que o println! porem retorna o valor
+        format!("Meu nome e {} e eu tenho {} anos", self.name, self.age)
+    } 
+}
 
 fn main() {
-    my_mod::nested::function();
-    my_mod::function();
+    let person = Person {name: "Bruno".to_string(), age: 32};
+    let person2 = Person {name: "Flavia".to_string(), age: 26};
+
+    println!("{}", person.sayhi());
+    print!("{}", person2.sayhi());
 }
+
+// mod my_mod;
+
+// fn main() {
+//     my_mod::nested::function();
+//     my_mod::function();
+// }
